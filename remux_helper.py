385459c6041e5ec41d5f80e71b59fa2bea29dd91
x-j <xjay12@gmail.com>
2019-06-11 20:29:39 +0200

    clean up parameter processing

--- remux_helper.py
+++ remux_helper.py
@@ -43,21 +43,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Help with remuxing files using OBS by cleaning up already remuxed MKV files.\nAnd also SFK and SFL files, for some reason.')
     parser.add_argument('target dir', type=str, help='the directory containing your MKV files')
-    parser.add_argument('-sd', '--straight_delete', dest="straight_delete",
-                        help="add this flag if you want to directly delete files instead of moving them to trash",
-                        action="store_true", required=False)
-    parser.add_argument('--skipSFK', dest='skip_sfk',
-                        help="add this flag if for some reason you don't want this script to delete your SFK and SFL files.",
-                        action="store_true", required=False)
-    parser.add_argument('--recurse', dest='recurse',
-                        help="add this flag to apply this script to all subdirectories of target dir",
-                        action="store_true", required=False)
+    parser.add_argument('-sd', '--straight_delete', dest="straight_delete", action="store_true", required=False,
+                        help="add this flag if you want to directly delete files instead of moving them to trash")
+    parser.add_argument('--skipSFK', dest='skip_sfk', action="store_true", required=False,
+                        help="add this flag if for some reason you don't want this script to delete your SFK and SFL files.")
+    parser.add_argument('--recurse', dest='recurse', action="store_true", required=False,
+                        help="add this flag to apply this script to all subdirectories of target dir")
 
     args = parser.parse_args()
     filex = None
 
-    straight_delete: bool = vars(args)["straight_delete"]         # <-- DANGEROUS CODE
-    straight_delete = True
+    straight_delete: bool = vars(args)["straight_delete"]  # <-- DANGEROUS CODE
 
     target_dir: str = vars(args)["target dir"]
     skip_sfk: bool = vars(args)["skip_sfk"]
